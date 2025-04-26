6aef636d3c39dc1935ae70dda311765a9b223b26
Daniel <danielatahanov053@gmail.com>
2025-04-26 10:37:37 +0500

    commit 8

--- js/slider.js
+++ js/slider.js
@@ -1,51 +1,47 @@
 $(document).ready(function(){
     $(".slider").slick({
-        arrows:false,
-        dots:false,
-        adaptiveHeight:true,
-        slidesToShow:1,
-        slidesToScroll:1,
-        speed:1000,
-        easing:"ease",
-        infinite:true,
-        initialSlide:0,
-        autoplay:true,
-        autoplaySpeed:3000,
-        pauseOneFocus:true,
-        pauseOnHover:true,
-        pauseOnDotsHover:true,
-        draggable:true,
-        swipe:true,
-        touchThreshold:10,
-        touchMove:true,
-        waitForAnimate:true,
-        centerMode:false,
-        variableWidth:false,
-        rows:1,
-        slidesPerRow:1,
-        vertical:false,
-        verticalSwiping:false,
-        fade:false,
-        mobileFirst:false,
-        // asNavFor:".связать слайды",
-        // appendArrows:$(".поместить сюда стрелки"),
-        // appendDots:$(".Поместить точик"),
-        responsive:[
+        arrows: false,
+        dots: false,
+        fade: true,  
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 900,
+        easing: "ease",
+        infinite: true,
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 3000, 
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        draggable: true,
+        swipe: false,
+        touchThreshold: 10,
+        touchMove: true,
+        waitForAnimate: true,
+        centerMode: false,
+        variableWidth: false,
+        rows: 1,
+        slidesPerRow: 1,
+        vertical: false,
+        verticalSwiping: false,
+        mobileFirst: false,
+        responsive: [
             {
                 breakpoint: 1300,
                 settings: {
-                     slidesToShow:2,
-                     slidesToScroll:2,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                 }
-            },{
+            },
+            {
                 breakpoint: 768,
                 settings: {
-                     slidesToShow:1,
-                     slidesToScroll:1,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             }
-        ],
-
+        ]
     });
-
 });
